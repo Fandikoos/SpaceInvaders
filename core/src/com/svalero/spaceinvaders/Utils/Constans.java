2d80228f4f68df7114497a56a2aff0d26d2108a9
Fandikoos <danfandos@gmail.com>
2024-04-03 14:03:19 +0200

    Add Configuration Manager and pause for Render Manager

---
+++ core/src/com/svalero/spaceinvaders/Utils/Constans.java
@@ -0,0 +1,13 @@
+package com.svalero.spaceinvaders.Utils;
+
+public class Constans {
+
+    public static final String GAME_NAME = "SpaceInvaders";
+
+    public static final String LOW = "LOW";
+    public static final String MEDIUM = "MEDIUM";
+    public static final String HARD = "HARD";
+
+    public static final String SOUND = "sound";
+    public static final String DIFFICULTY = "difficulty";
+}
--- core/src/com/svalero/spaceinvaders/manager/ConfigurationManager.java
+++ core/src/com/svalero/spaceinvaders/manager/ConfigurationManager.java
@@ -3,20 +3,41 @@ package com.svalero.spaceinvaders.manager;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Preferences;
 
+import static com.svalero.spaceinvaders.Utils.Constans.*;
+
 public class ConfigurationManager {
 
-    private static Preferences prefs = Gdx.app.getPreferences("SpaceInvaders");
+    private static Preferences prefs = Gdx.app.getPreferences(GAME_NAME);
 
     public static boolean isSoundAlreadyConfigured() {
-        return prefs.contains("sound");
+        return prefs.contains(SOUND);
     }
 
     public static boolean isSoundEnabled() {
-        return prefs.getBoolean("sound");
+        return prefs.getBoolean(SOUND);
     }
 
     public static void enableSound() {
-        prefs.putBoolean("sound", true);
+        prefs.putBoolean(SOUND, true);
+        prefs.flush();
+    }
+
+    public static void disableSound() {
+        prefs.putBoolean(SOUND, false);
+        prefs.flush();
+    }
+
+    public static void switchSound(boolean value) {
+        prefs.putBoolean(SOUND, value);
+        prefs.flush();
+    }
+
+    public static String getDifficulty() {
+        return prefs.getString(DIFFICULTY);
+    }
+
+    public static void setDifficulty(String value) {
+        prefs.putString(DIFFICULTY, value);
         prefs.flush();
     }
 
